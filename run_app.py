19e1b3c1e4b7de68167d8267219a347fe36cf912
chain-smok <s1101780@gm.pu.edu.tw>
2023-03-16 01:20:38 +0800

    big change

---
+++ run_app.py
@@ -0,0 +1,12 @@
+import os
+
+
+from library._05_basic_routing import app
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    host = os.environ.get('IP', '0.0.0.0')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host=host, port=port)
