703b65371418f1d2cef6df6ad06c8cde28634e46
chain-smok <s1101780@gm.pu.edu.tw>
2023-03-15 13:57:51 +0800

    first commit

---
+++ basic_routing.py
@@ -0,0 +1,12 @@
+import json
+
+with open('HW4.json') as f:
+  info = json.load(f)
+
+print('school:',info['school'])
+print('\nteacher:',info['teacher'])
+print('\nProjectname:',info['Projectname'])
+print('\n組員資料')
+print('_____________________________________________')
+for members in info['member']:
+    print("名字:%s 學號:%s 性別:%s"%(members['name'],members['number'],members['gender']))
